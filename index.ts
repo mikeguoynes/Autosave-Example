439dcc075c7209a04658f2c435e99179e22c35cd
mikeguoynes <mikeguoynes@gmail.com>
2021-08-11 23:32:28 -0500

    Autosave example with counts to compare w/ debounce vs without

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
 import './style.css';
-
 import { of, map, fromEvent, debounceTime } from 'rxjs';
 
 const searchField = document.getElementById('search');
 const typing$ = fromEvent(searchField, 'keyup');
 let noDebounceRequests = 0;
 let withDebounceRequests = 0;
-typing$.subscribe(res => {
+
+typing$.subscribe(() => {
   noDebounceRequests++;
   document.getElementById('result-1').textContent = noDebounceRequests;
 });
 
 // Delay 300 ms from after the events complete w/ debounceTime
-typing$.pipe(debounceTime(300)).subscribe(res => {
-  console.log('Sent Request to autosave WITH debouncing'); // Open the console in the bottom right to see results.
+typing$.pipe(debounceTime(300)).subscribe(() => {
   withDebounceRequests++;
   document.getElementById('result-2').textContent = withDebounceRequests;
 });
+
+document.getElementById('result-1').textContent = noDebounceRequests;
+document.getElementById('result-2').textContent = withDebounceRequests;
